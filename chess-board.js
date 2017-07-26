9daae64bf34715babee27fe8920743c7ebd369be
dbillings <andrewandroid@gmail.com>
2017-07-26 16:29:28 -0700

    refactor code to increase usability by moving helper functions out of generateboard function.

--- chess-board.js
+++ chess-board.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var generateBoard = function(width, height, char1, char2) {    
     var area = height * width;
     var nl   = "\n";
@@ -32,6 +33,31 @@ var generateBoard = function(width, height, char1, char2) {
     for(i = 0; i < height/2; i++)
         board += slice;
 
+=======
+var mkLine = function(width, char1, char2){
+    var line = "";
+    for(i = 0; i < width; i++)
+        if (line.slice(-1) == char1) {
+            line += char2;
+        } else {
+            line += char1;
+        } 
+        return line;
+};
+var reverseStr = function(str) {
+    for (var i = str.length - 1, revStr = ''; i >= 0; revStr += str[i--]) { }
+    return revStr;
+};
+var generateBoard = function(width, height, char1, char2) {    
+    var area = height * width;
+    var nl   = "\n";
+    var board = ""; 
+    var line = mkLine(width,char1,char2);
+    var slice = line+nl+reverseStr(line)+nl;
+
+    for(i = 0; i < height/2; i++)
+        board += slice; 
+>>>>>>> Stashed changes
     return board;
 };
 
