e37d17c85a2496834a8be5a7c8439e06de3ee1e1
dbillings <andrewandroid@gmail.com>
2017-07-26 16:37:16 -0700

    clean up merge

--- chess-board.js
+++ chess-board.js
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-var generateBoard = function(width, height, char1, char2) {    
-    var area = height * width;
-    var nl   = "\n";
-    var board = ""; 
-    var line1 = function(width, char1, char2){
-        var line = "";
-        for(i = 0; i < width; i++)
-        if (line.slice(-1) == char1) {
-            line += char2;
-        } else if (line.slice(-1) != nl){
-            line += char1;
-        }
-        line += nl;
-        return line;
-    };
-    var line2 = function(width, char1, char2){
-        var line = "";
-        for(i = 0; i < width; i++)
-        if (line.slice(-1) == char2) {
-            line += char1;
-            
-        } else if (line.slice(-1) != nl){
-            line += char2;
-        }
-        line += nl;
-        return line;
-    };
-
-
-    var slice = line1(width, char1, char2) + line2(width, char1, char2);
-
-    for(i = 0; i < height/2; i++)
-        board += slice;
-
-=======
 var mkLine = function(width, char1, char2){
     var line = "";
     for(i = 0; i < width; i++)
@@ -57,7 +21,6 @@ var generateBoard = function(width, height, char1, char2) {
 
     for(i = 0; i < height/2; i++)
         board += slice; 
->>>>>>> Stashed changes
     return board;
 };
 
