74bdf13906df32bfbddf967677ddf2d3d9c9a699
dbillings <andrewandroid@gmail.com>
2017-07-25 18:54:09 -0700

    add a generateboard funtion that takes width, height, and characters to use, as arguments.

--- chess-board.js
+++ chess-board.js
@@ -1,28 +1,38 @@
-alternateChars = function(nChar, char1, char2){
-    var line = ""
-    
-    for(i = 0; i <= nChar; i++)
+var generateBoard = function(w, h, c1, c2) {    
+    var area = h * w;
+    var nl   = "\n";
+    var board = ""; 
+    var line1 = function(width, char1, char2){
+        var line = "";
+        for(i = 0; i <= width; i++)
         if (line.slice(-1) == char1) {
             line += char2;
-        
-        } else {
+        } else if (line.slice(-1) != nl){
             line += char1;
         }
-    return line;
-};
-addNewLine = function(rawline){
-    newlineLine = rawline + "\n"
-    return newlineLine;
-};
-
-printLines = function(nLines, line){
-    var lines = "";
-    for(i = 0; i < nLines; i++){
-        
-        lines += line
-    
+        line += nl;
+        return line;
     };
-    return lines;
+    var line2 = function(width, char1, char2){
+        var line = "";
+        for(i = 0; i <= width; i++)
+        if (line.slice(-1) == char2) {
+            line += char1;
+            
+        } else if (line.slice(-1) != nl){
+            line += char2;
+        }
+        line += nl;
+        return line;
+    };
 
+
+    var slice = line1(w, c1, c2) + line2(w, c1, c2);
+
+    for(i = 0; i <= h/2; i++)
+        board += slice;
+
+    return board;
 };
-console.log(printLines(10, addNewLine(alternateChars(10, "3", " "))));
+
+console.log(generateBoard(69,1000,"f","u"));
